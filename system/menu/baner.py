91964c58ba8b4f0ffc82bb33520978d2cbaa18a0
pyadrus <zh.vitaliy92@gmail.com>
2023-04-17 23:33:42 +0300

    Изменение версии программы

--- system/menu/baner.py
+++ system/menu/baner.py
@@ -1,7 +1,7 @@
 from rich.console import Console
 
 console = Console()
-program_version, date_of_program_change = "0.8.5", "05.04.2023"  # Версия программы, дата изменения
+program_version, date_of_program_change = "0.8.6", "17.04.2023"  # Версия программы, дата изменения
 
 
 def banner() -> None:
