d43d738ee4517b202d7cfac3936a27399db3d093
pyadminrus <zh.vitaliy92@gmail.com>
2023-11-09 17:10:51 +0300

    Незначительные улучшения

--- system/menu/app_banner.py
+++ system/menu/app_banner.py
@@ -1,7 +1,7 @@
 from rich.console import Console
 
 console = Console()
-program_version, date_of_program_change = "0.9.7", "09.10.2023"  # Версия программы, дата изменения
+program_version, date_of_program_change = "0.9.8", "09.11.2023"  # Версия программы, дата изменения
 
 
 def banner() -> None:
