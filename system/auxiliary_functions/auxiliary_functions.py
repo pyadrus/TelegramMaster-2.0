e0ca7d3cd369e4287bf82a3bc974b6fc7f42f7e2
PyAdminRU <zh.vitaliy92@gmail.com>
2024-06-01 16:16:43 +0300

    Удаление зависимостей от библиотеки rich + рефакторинг кода.

--- system/auxiliary_functions/auxiliary_functions.py
+++ system/auxiliary_functions/auxiliary_functions.py
@@ -7,8 +7,6 @@ import time  # Импортируем модуль time, чтобы работа
 from sys import platform
 
 from loguru import logger
-from rich.progress import track
-
 
 from system.auxiliary_functions.global_variables import ConfigReader
 from system.error.telegram_errors import record_account_actions
@@ -20,19 +18,23 @@ time_inviting_1, time_inviting_2 = configs_reader.get_time_inviting()
 
 
 def read_json_file(filename):
-    """Чтение данных из файла JSON.
+    """
+    Чтение данных из файла JSON.
     :param filename: Полный путь к файлу JSON.
-    :return: Данные из файла JSON в виде словаря."""
+    :return: Данные из файла JSON в виде словаря.
+    """
     with open(filename, 'r', encoding="utf-8") as file:
         data = json.load(file)
     return data
 
 
 def find_files(directory_path, extension) -> list:
-    """Поиск файлов с определенным расширением в директории.
+    """
+    Поиск файлов с определенным расширением в директории.
     :arg directory_path: - путь к директории
     :arg extension: - расширение файла
-    :return list: - список имен найденных файлов"""
+    :return list: - список имен найденных файлов
+    """
 
     entities = []  # Создаем словарь с именами найденных аккаунтов в папке user_settings/accounts
     for x in os.listdir(directory_path):
@@ -44,28 +46,40 @@ def find_files(directory_path, extension) -> list:
     return entities  # Возвращаем список json файлов
 
 
-def display_progress_bar(time_range_1, time_range_2, message) -> None:
-    """Отображаем время смены аккаунта или username в виде progress bar"""
-    # Генерируем случайное число в указанном диапазоне времени
-    selected_shift_time = random.randrange(time_range_1, time_range_2)
-    for _ in track(range(selected_shift_time),
-                   description=f"[red]{message} через {selected_shift_time} секунды/секунд..."):
-        time.sleep(1)
-
-
-def record_inviting_results(username, description_action, event, actions, db_handler) -> None:
-    """Запись результатов inviting, отправка сообщений в базу данных"""
+def record_inviting_results(time_range_1, time_range_2, username, description_action, event, actions, db_handler) -> None:
+    """
+    Запись результатов inviting, отправка сообщений в базу данных.
+    :param time_range_1:  - диапазон времени смены аккаунта
+    :param time_range_2:  - диапазон времени смены аккаунта
+    :param username: - username аккаунта
+    :param description_action: - описание действия
+    :param event: - событие
+    :param actions: - список действий
+    :param db_handler: - объект класса БД
+    """
     record_account_actions(description_action, event, actions, db_handler)
     db_handler.delete_row_db(table="members", column="username", value=username)
     # Смена username через случайное количество секунд
-    display_progress_bar(time_inviting_1, time_inviting_2, "Переход к новому username")
+    selected_shift_time = random.randrange(time_range_1, time_range_2)
+    print(f"Переход к новому username через {selected_shift_time} секунд")
+    time.sleep(selected_shift_time)
 
 
-def record_and_interrupt(actions, description_action, event, db_handler) -> None:
-    """Запись данных в базу данных и прерывание выполнения кода"""
+def record_and_interrupt(actions, description_action, event, db_handler, time_range_1, time_range_2) -> None:
+    """
+    Запись данных в базу данных и прерывание выполнения кода.
+    :param time_range_1:  - диапазон времени смены аккаунта
+    :param time_range_2:  - диапазон времени смены аккаунта
+    :param actions:  - список действий
+    :param description_action:  - описание действия
+    :param event:  - событие
+    :param db_handler:  - объект класса БД
+    """
     record_account_actions(description_action, event, actions, db_handler)
     # Смена аккаунта через случайное количество секунд
-    display_progress_bar(time_changing_accounts_1, time_changing_accounts_2, "Ожидайте смены аккаунта")
+    selected_shift_time = random.randrange(time_range_1, time_range_2)
+    print(f"Переход к новому username через {selected_shift_time} секунд")
+    time.sleep(selected_shift_time)
 
 
 def clear_console_and_display_banner() -> None:
@@ -75,12 +89,6 @@ def clear_console_and_display_banner() -> None:
     else:
         os.system("clear")  # Чистим консоль (для linux clear)
     banner()  # Ставим банер программы, для красивого визуального отображения
-
-
-def column_names(table) -> None:
-    """Название столбцов, для меню программы"""
-    table.add_column("[medium_purple3]№ функции", justify="left", style="cyan")
-    table.add_column("[medium_purple3]Функция", justify="left", style="sandy_brown")
 
 
 if __name__ == "__main__":
