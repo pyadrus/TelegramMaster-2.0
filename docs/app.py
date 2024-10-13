059121f00fe4360aea4aee26e9a863382b7079a5
PyAdminRU <zh.vitaliy92@gmail.com>
2024-10-14 00:42:42 +0300

    ⬆️ Рефакторинг документации

--- docs/app.py
+++ docs/app.py
@@ -8,7 +8,7 @@ from watchfiles import awatch
 
 app = Quart(__name__, template_folder='templates')
 
-program_version, date_of_program_change = "2.2.6", "11.10.2024"  # Версия программы, дата изменения
+program_version, date_of_program_change = "2.2.7", "13.10.2024"  # Версия программы, дата изменения
 
 
 @app.route('/')
