a732d0f95528277d11ee86cd83a403ac63eb08e2
@PyAdminRU <zh.vitaliy92@gmail.com>
2024-11-06 00:15:44 +0300

    ⬆️ Добавлена проверка целостности структуры проекта на наличие файлов.

--- setup.py
+++ setup.py
@@ -1,3 +1,29 @@
 import os
 
 os.system("""pip install -r requirements.txt""")
+
+
+# Список всех необходимых директорий
+directories = [
+    'user_settings/accounts/answering_machine',
+    'user_settings/accounts/banned',
+    'user_settings/accounts/bio',
+    'user_settings/accounts/contact',
+    'user_settings/accounts/creating',
+    'user_settings/accounts/inviting',
+    'user_settings/accounts/parsing',
+    'user_settings/accounts/reactions',
+    'user_settings/accounts/reactions_list',
+    'user_settings/accounts/send_message',
+    'user_settings/accounts/subscription',
+    'user_settings/accounts/unsubscribe',
+    'user_settings/accounts/viewing'
+]
+
+for directory in directories:
+    try:
+        # Создаём директорию с возможностью игнорирования ошибки существования
+        os.makedirs(directory, exist_ok=True)
+        print(f"Папка '{directory}' создана")
+    except OSError as e:
+        print(f"Ошибка создания папки '{directory}': {e}")
