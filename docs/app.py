1d1d86045f0b1f89da37b892cbe9ed280c548653
PyAdminRU <zh.vitaliy92@gmail.com>
2024-12-19 00:27:58 +0300

    ⬆️ Раздел документации "📨 Лимиты на сообщения" был вынесен в отдельную деррикторию.

--- docs/app.py
+++ docs/app.py
@@ -24,7 +24,7 @@ from system.localization.localization import (parse_selected_user_subscribed_gro
                                               sending_files_to_personal_account_with_limits, choice_of_reactions_ru,
                                               proxy_entry_ru, changing_accounts_ru, recording_api_id_api_hash_ru,
                                               time_between_subscriptions_ru, message_recording_ru, link_entry_ru, account_limits_ru,
-                                              message_limits, time_between_subscriptionss, creating_username_list,
+                                              message_limits_ru, time_between_subscriptionss, creating_username_list,
                                               recording_the_time_between_messages,
                                               time_between_invites_sending_messages, recording_reaction_link,
                                               forming_list_of_chats_channels, changing_the_username, changing_the_photo,
@@ -122,7 +122,7 @@ async def settings(request: Request):
                                                         "message_recording_ru": message_recording_ru,
                                                         "link_entry_ru": link_entry_ru,
                                                         "account_limits_ru": account_limits_ru,
-                                                        "message_limits": message_limits,
+                                                        "message_limits_ru": message_limits_ru,
                                                         "time_between_subscriptionss": time_between_subscriptionss,
                                                         "creating_username_list": creating_username_list,
                                                         "recording_the_time_between_messages": recording_the_time_between_messages,
@@ -215,6 +215,15 @@ async def account_limits(request: Request):
         "account_limits_ru": account_limits_ru,
     })
 
+@app.get('/settings/message_limits', response_class=HTMLResponse)
+async def message_limits(request: Request):
+    """📨 Лимиты на сообщения"""
+    logger.info("Запущена страница 📨 Лимиты на сообщения")
+    return templates.TemplateResponse('settings/message_limits.html', {
+        "request": request,
+        "program_name": program_name,
+        "message_limits_ru": message_limits_ru,
+    })
 
 
 # Работа с реакциями
--- system/menu_gui/menu_gui.py
+++ system/menu_gui/menu_gui.py
@@ -16,7 +16,7 @@ from system.localization.localization import (parse_single_or_multiple_groups, p
                                               sending_files_to_personal_account_with_limits, choice_of_reactions_ru,
                                               proxy_entry_ru, changing_accounts_ru, recording_api_id_api_hash_ru,
                                               time_between_subscriptions_ru, message_recording_ru, link_entry_ru, account_limits_ru,
-                                              message_limits, time_between_subscriptionss, creating_username_list,
+                                              message_limits_ru, time_between_subscriptionss, creating_username_list,
                                               recording_the_time_between_messages,
                                               time_between_invites_sending_messages, recording_reaction_link,
                                               forming_list_of_chats_channels, we_are_winding_up_post_views, editing_bio,
@@ -91,7 +91,7 @@ async def settings_menu(page):
                      ft.Row([
                          # 📨 Лимиты на сообщения
                          ft.ElevatedButton(width=small_button_width, height=height_button,
-                                           text=message_limits,
+                                           text=message_limits_ru,
                                            on_click=lambda _: page.go("/message_limits")),
                          # ⏳ Время между подпиской
                          ft.ElevatedButton(width=small_button_width, height=height_button,
